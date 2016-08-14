287369afc73aed0a07fdcb41274ca3f01cf25994
marclos <marc.loshuertos@pomona.edu>
2016-08-13 20:10:39 -0700

    working on functions

--- 32_Soil_Texture_Analysis/Soil_functions/SoilEquiv_fun.R
+++ 32_Soil_Texture_Analysis/Soil_functions/SoilEquiv_fun.R
@@ -51,36 +51,72 @@ Et_aligning(c(0.5, 2, 30,60, 150, 1440))
 # Function to Impute Temperature
 imputeT <- function(sampleID){
   library(dplyr)
-  tmp <- sampleID %>% select(Et, Rblank, Temp)
-  tmp$Et_Align <- Et_aligning(Test$Et) 
+  tmp2 <- sampleID %>% select(Et, blank, temp)
+  #tmp2 <- tmp %>% select(Et, blank, temp); tmp2
   
-  # non missing, keeping Rblank and Et_Align
-  nonmissingR = tmp[!is.na(tmp$Rblank),c(2,4)]; nonmissingR 
-  missingR = tmp[is.na(tmp$Rblank),c(2,4)]; missingR
-  nonmissingT = tmp[!is.na(tmp$Temp),c(3,4)]; nonmissingT
-  missingT = tmp[is.na(tmp$Temp),c(3,4)]; missingT
+  missingT = tmp2[is.na(tmp2$temp),]; missingT
+  nonmissingT = tmp2[!is.na(tmp2$temp),]; nonmissingT
   
-  tmpR = left_join(missingR, nonmissingR, by="Et_Align")
-  tmpT = left_join(missingT, nonmissingT, by="Et_Align")
+  missing.mat <- matrix(missingT$Et, nrow=length(missingT$Et), ncol=length(nonmissingT$Et), byrow=F); missing.mat
+  nonmissing.mat <- matrix(nonmissingT$Et, nrow=length(missingT$Et), ncol=length(nonmissingT$Et), byrow=T); nonmissing.mat
   
-  tmpT$Temp.y
+  W <- abs(missing.mat - nonmissing.mat)
+  rownames(W) <- paste0("Missing", seq(nrow(W)))
+  colnames(W) <- paste0("NonMissing", seq(ncol(W)))
+  
+  result <- t(sapply(seq(nrow(W)), function(i) {
+    j <- which.min(W[i,])
+    c(i, j)
+  }))
+  
+  alignments <- data.frame(missing = result[,1], nonmissing=result[,2])
+  #print(result)
+  
+return(as.vector(nonmissingT[result[,2],3]))
 }
 
+# Function to impute Reading Blanks
 imputeR <- function(sampleID){
   library(dplyr)
-  tmp <- sampleID %>% select(Et, Rblank, Temp)
-  tmp$Et_Align <- Et_aligning(Test$Et) 
+  tmp2 <- sampleID %>% select(Et, blank, temp)
+  
+  missingR = tmp2[is.na(tmp2$blank),]; missingR
+  nonmissingR = tmp2[!is.na(tmp2$blank),]; nonmissingR
+  
+  missing.mat <- matrix(missingR$Et, nrow=length(missingR$Et), ncol=length(nonmissingR$Et), byrow=F); missing.mat
+  nonmissing.mat <- matrix(nonmissingR$Et, nrow=length(missingR$Et), ncol=length(nonmissingR$Et), byrow=T); nonmissing.mat
+  
+  W <- abs(missing.mat - nonmissing.mat)
+  rownames(W) <- paste0("Missing", seq(nrow(W)))
+  colnames(W) <- paste0("NonMissing", seq(ncol(W)))
+  
+  result <- t(sapply(seq(nrow(W)), function(i) {
+    j <- which.min(W[i,])
+    c(i, j)
+  }))
+  
+  alignments <- data.frame(missingR = result[,1], nonmissingR=result[,2])
+  #print(result)
+  
+  return(as.vector(nonmissingR[result[,2],2]))
+}
+
+# Old version!
+#imputeR <- function(sampleID){
+#  library(dplyr)
+#  tmp <- sampleID %>% select(Et, Rblank, Temp)
+#  tmp$Et_Align <- Et_aligning(Test$Et) 
   
   # non missing, keeping Rblank and Et_Align
-  nonmissingR = tmp[!is.na(tmp$Rblank),c(2,4)]; nonmissingR 
-  missingR = tmp[is.na(tmp$Rblank),c(2,4)]; missingR
-  nonmissingT = tmp[!is.na(tmp$Temp),c(3,4)]; nonmissingT
-  missingT = tmp[is.na(tmp$Temp),c(3,4)]; missingT
+#  nonmissingR = tmp[!is.na(tmp$Rblank),c(2,4)]; nonmissingR 
+#  missingR = tmp[is.na(tmp$Rblank),c(2,4)]; missingR
+#  nonmissingT = tmp[!is.na(tmp$Temp),c(3,4)]; nonmissingT
+#  missingT = tmp[is.na(tmp$Temp),c(3,4)]; missingT
   
-  tmpR = left_join(missingR, nonmissingR, by="Et_Align")
-  tmpT = left_join(missingT, nonmissingT, by="Et_Align")
-  tmpR$Rblank.y
-}
+#  tmpR = left_join(missingR, nonmissingR, by="Et_Align")
+#  tmpT = left_join(missingT, nonmissingT, by="Et_Align")
+#  tmpR$Rblank.y
+#}
 
 # Function to Select K
 Kfun <- function(T=22, GSp = 2.65){
