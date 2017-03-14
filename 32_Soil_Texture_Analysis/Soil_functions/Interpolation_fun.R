ed1d90fad795f56ac56126107421796555ee994a
marclos <marc.loshuertos@pomona.edu>
2017-03-14 08:39:54 -0700

    fixed parts of code

--- 32_Soil_Texture_Analysis/Soil_functions/Interpolation_fun.R
+++ 32_Soil_Texture_Analysis/Soil_functions/Interpolation_fun.R
@@ -1,14 +1,18 @@
 # Function to Interpolate the Texture Class
-Interpolation <- function(tmp3, texturebreak){
+Interpolation <- function(tmp4, texturebreak){
+  # Vector of De smaller than criteria
+  De_min1 = tmp4 %>% filter(tmp4$De < texturebreak); De_min1
   
-  De_min1 = tmp3 %>% filter(tmp3$De < texturebreak); De_min1
+  # Vector of largest remaining De
   De_min2 = De_min1 %>% filter(De_min1$De == max(De_min1$De)); De_min2
   
-  De_max1 = tmp3 %>% filter(tmp3$De > texturebreak); De_max1
+  # Vector of De larger than criteria
+  De_max1 = tmp4 %>% filter(tmp4$De > texturebreak); De_max1
+  # Vector of smallest remaining De
   De_max2 = De_max1 %>% filter(De_max1$De == min(De_max1$De)); De_max2
   
-  x <- c(De_min2[,2], De_max2[,2]); x
-  y <- c(De_min2[,3], De_max2[,3]); y
+  x <- c(De_min2[,2], De_max2[,2]); x # De
+  y <- c(De_min2[,3], De_max2[,3]); y # PF
   
   m = lm(y ~ x)$coeff[2] 
   b = lm(y ~ x)$coeff[1]
